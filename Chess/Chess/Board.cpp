e8fedb3563178e9a046e70dc0cb939fa5d8baf63
mruiz <mruiz@ERC107-06>
2016-10-13 13:46:51 -0400

    saved

--- Chess/Chess/Board.cpp
+++ Chess/Chess/Board.cpp
@@ -2,36 +2,43 @@
 
 void Board::display_board()
 {
-	//Here I will chose how I want to display my board.
-	for (int row = 0; row < board_size; ++row)
-	{
-		for (int col = 0; col < board_size; col++)
-		{
-			cout << "-----";
-		}
-		cout << "-" << endl;
-		for (int col = 0; col < board_size; col++)
-		{
-			cout << "|" << "    ";
-		}
-		cout << "|" << endl;
-		for (int col = 0; col < board_size; col++)
-		{
-			cout << "|" << "    ";
-		}
-		cout << "|" << endl;
-		for (int col = 0; col < board_size; col++)
-		{
-			cout << "|" << "    ";
-		}
-		cout << "|" << endl;
-	}
 	for (int col = 0; col < board_size; col++)
 	{
-		cout << "-----";
+		cout << " " << "|";
+		cout << " " << board[0][0]->getPiece();
+		cout << " " << "|";
 	}
-	cout << "-" << endl;
 	
+	////Here I will chose how I want to display my board.
+	//for (int row = 0; row < board_size; ++row)
+	//{
+	//	for (int col = 0; col < board_size; col++)
+	//	{
+	//		cout << "-----";
+	//	}
+	//	cout << "-" << endl;
+	//	for (int col = 0; col < board_size; col++)
+	//	{
+	//		cout << "|" << "    ";
+	//	}
+	//	cout << "|" << endl;
+	//	for (int col = 0; col < board_size; col++)
+	//	{
+	//		cout << "|" << "    ";
+	//	}
+	//	cout << "|" << endl;
+	//	for (int col = 0; col < board_size; col++)
+	//	{
+	//		cout << "|" << "    ";
+	//	}
+	//	cout << "|" << endl;
+	//}
+	//for (int col = 0; col < board_size; col++)
+	//{
+	//	cout << "-----";
+	//}
+	//cout << "-" << endl;
+	//
 }
 
 void Board::display_piece()
